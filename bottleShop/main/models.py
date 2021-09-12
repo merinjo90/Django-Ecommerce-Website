0f0a3b3f1cccff8ca68816581669f3d422d8043a
merinjo90 <merinjo90@gmail.com>
2021-09-12 15:20:29 -0400

    model created name is Category

--- bottleShop/main/models.py
+++ bottleShop/main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    title=models.CharField(max_length=100)
+    image=models.ImageField(upload_to="cat_img/")
+
+    def __str__(self):
+        return self.title
