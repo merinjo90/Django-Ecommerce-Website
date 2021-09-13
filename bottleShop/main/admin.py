f9139c0cfdcea9614181930c2c6f277e7aca257c
merinjo90 <merinjo90@gmail.com>
2021-09-13 01:36:12 -0400

    list_editable [add status] status editable

--- bottleShop/main/admin.py
+++ bottleShop/main/admin.py
@@ -9,4 +9,5 @@ admin.site.register(Size)
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('id','title','brand','color','size','price','status')
+    list_editable = ('status',)
 admin.site.register(Product,ProductAdmin)
