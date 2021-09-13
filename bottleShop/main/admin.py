26ca833deb685dac048f286c2c509c8ffc7eb525
merinjo90 <merinjo90@gmail.com>
2021-09-13 01:16:57 -0400

    Product model register in admin panel

--- bottleShop/main/admin.py
+++ bottleShop/main/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Category,Brand,Color,Size
+from .models import Category,Brand,Color,Size,Product
 
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Brand)
 admin.site.register(Color)
 admin.site.register(Size)
+admin.site.register(Product)
