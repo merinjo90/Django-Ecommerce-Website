daaf1de82a2670743f0c4531df5dcb1d13bf6006
merinjo90 <merinjo90@gmail.com>
2021-09-12 22:27:34 -0400

    model created in main app [main/models.py] Brand,Color,Size are the model name

--- bottleShop/main/models.py
+++ bottleShop/main/models.py
@@ -1,9 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+#category
 class Category(models.Model):
     title=models.CharField(max_length=100)
     image=models.ImageField(upload_to="cat_img/")
+
+    def __str__(self):
+        return self.title
+
+#brand
+class Brand(models.Model):
+    title=models.CharField(max_length=100)
+    image=models.ImageField(upload_to="brand_img/")
+
+    def __str__(self):
+        return self.title
+
+#Color
+class Color(models.Model):
+    title=models.CharField(max_length=100)
+    color_cod=models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+
+#Size
+class Size(models.Model):
+    title=models.CharField(max_length=100)
 
     def __str__(self):
         return self.title
