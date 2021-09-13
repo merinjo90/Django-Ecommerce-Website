809cc668bdb975c4d39b7ee8ec77ce43c1f4cdc5
merinjo90 <merinjo90@gmail.com>
2021-09-13 01:32:33 -0400

    create class ProductAdmin and extended class,list_disply[define the list,show content] show the price and status

--- bottleShop/main/admin.py
+++ bottleShop/main/admin.py
@@ -6,4 +6,7 @@ admin.site.register(Category)
 admin.site.register(Brand)
 admin.site.register(Color)
 admin.site.register(Size)
-admin.site.register(Product)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id','title','brand','color','size','price','status')
+admin.site.register(Product,ProductAdmin)
