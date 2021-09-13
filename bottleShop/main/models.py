2d37dad0ee5273006c8619264b5dad5a98d11bdc
merinjo90 <merinjo90@gmail.com>
2021-09-13 01:15:51 -0400

    Product model created in main app,[brand,category,color,size are Foreign key]

--- bottleShop/main/models.py
+++ bottleShop/main/models.py
@@ -32,3 +32,21 @@ class Size(models.Model):
 
     def __str__(self):
         return self.title
+
+#Product Model
+class Product(models.Model):
+    title=models.CharField(max_length=200)
+    image=models.ImageField(upload_to='product_img/')
+    slug=models.CharField(max_length=400)
+    detail=models.TextField()
+    specifs=models.TextField()
+    price=models.PositiveIntegerField()
+    brand=models.ForeignKey(Brand,on_delete=models.CASCADE)
+    category=models.ForeignKey(Category,on_delete=models.CASCADE)
+    color=models.ForeignKey(Color,on_delete=models.CASCADE)
+    size=models.ForeignKey(Size,on_delete=models.CASCADE)
+    status=models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
