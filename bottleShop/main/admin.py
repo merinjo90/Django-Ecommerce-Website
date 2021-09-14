e9809f796cf28f5c365d3a130eb5185e3ca17fc4
merinjo90 <merinjo90@gmail.com>
2021-09-14 01:11:01 -0400

    ProductAttribute is register on admin panel

--- bottleShop/main/admin.py
+++ bottleShop/main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category,Brand,Color,Size,Product
+from .models import Category,Brand,Color,Size,Product,ProductAttribute
 
 # Register your models here.
 admin.site.register(Category)
@@ -8,6 +8,11 @@ admin.site.register(Color)
 admin.site.register(Size)
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id','title','brand','color','size','price','status')
+    list_display = ('id','title','brand','color','size','status')
     list_editable = ('status',)
 admin.site.register(Product,ProductAdmin)
+
+#product attribute
+class ProductAttributeAdmin(admin.ModelAdmin):
+    list_display = ('id','product','color','size','price')
+admin.site.register(ProductAttribute,ProductAttributeAdmin)
