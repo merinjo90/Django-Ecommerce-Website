31272d896dfd2bf6d8b8e3cc33c5c5d9cf287f45
merinjo90 <merinjo90@gmail.com>
2021-09-15 00:48:13 -0400

    create category list page,all category with images and title.we will create views and lay out,we will add admin panel to add data, create layout [fetch data from data base]

--- bottleShop/main/views.py
+++ bottleShop/main/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from .models import Category
 
 # Create your views here.
 
 #Home Page
 def home(request):
     return render(request,'index.html')
+
+#Category
+def category_list(request):
+    data=Category.objects.all().order_by('-id')
+    return render(request,'category_list.html',{'data':data})
+
+#Brand
