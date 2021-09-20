e11256b5367ed498fead94a6883e620fc4cc430b
merinjo90 <merinjo90@gmail.com>
2021-09-20 01:44:15 -0400

    Reset the order of the models

--- bottleShop/main/models.py
+++ bottleShop/main/models.py
@@ -2,10 +2,21 @@ from django.db import models
 
 # Create your models here.
 
+# Banner
+class Banner(models.Model):
+    img = models.CharField(max_length=200)
+    alt_text = models.CharField(max_length=300)
+
+    class Meta:
+        verbose_name_plural='1. Banners'
+
 #category
 class Category(models.Model):
     title=models.CharField(max_length=100)
     image=models.ImageField(upload_to="cat_img/")
+
+    class Meta:
+        verbose_name_plural='2. Categories'
 
     def __str__(self):
         return self.title
@@ -15,6 +26,9 @@ class Brand(models.Model):
     title=models.CharField(max_length=100)
     image=models.ImageField(upload_to="brand_img/")
 
+    class Meta:
+        verbose_name_plural='3. Brands'
+
     def __str__(self):
         return self.title
 
@@ -23,6 +37,9 @@ class Color(models.Model):
     title=models.CharField(max_length=100)
     color_cod=models.CharField(max_length=100)
 
+    class Meta:
+        verbose_name_plural='4. Colors'
+
     def __str__(self):
         return self.title
 
@@ -30,13 +47,12 @@ class Color(models.Model):
 class Size(models.Model):
     title=models.CharField(max_length=100)
 
+    class Meta:
+        verbose_name_plural='5. Size'
+
     def __str__(self):
         return self.title
 
-# Banner
-class Banner(models.Model):
-    img = models.CharField(max_length=200)
-    alt_text = models.CharField(max_length=300)
 
 #Product Model
 class Product(models.Model):
@@ -52,6 +68,9 @@ class Product(models.Model):
     size=models.ForeignKey(Size,on_delete=models.CASCADE)
     status=models.BooleanField(default=True)
 
+    class Meta:
+        verbose_name_plural='6. Products'
+
     def __str__(self):
         return self.title
 
@@ -61,6 +80,9 @@ class ProductAttribute(models.Model):
     color=models.ForeignKey(Color,on_delete=models.CASCADE)
     size=models.ForeignKey(Size,on_delete=models.CASCADE)
     price=models.PositiveIntegerField()
+
+    class Meta:
+        verbose_name_plural='7. ProductAttribute'
 
     def __str__(self):
         return self.product.title
