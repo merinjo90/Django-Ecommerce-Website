b4a1f77a8fd3d309e2307d2eb27e19bb4bed5fb0
merinjo90 <merinjo90@gmail.com>
2021-09-20 02:55:08 -0400

    customize django Admin

--- bottleShop/main/admin.py
+++ bottleShop/main/admin.py
@@ -4,12 +4,16 @@ from .models import Banner,Category,Brand,Color,Size,Product,ProductAttribute
 # Register your models here.
 admin.site.register(Banner)
 admin.site.register(Brand)
-admin.site.register(Color)
 admin.site.register(Size)
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('title','image_tag')
 admin.site.register(Category,CategoryAdmin)
+
+class ColorAdmin(admin.ModelAdmin):
+    list_display = ('title','color_bg')
+admin.site.register(Color,ColorAdmin)
+
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('id','title','brand','color','size','status')
