bffec46f1449db6b0e0bb3baef6bba9811f13d01
merinjo90 <merinjo90@gmail.com>
2021-09-20 19:38:02 -0400

    connecting the urls.py file with the views.py file

--- bottleShop/main/urls.py
+++ bottleShop/main/urls.py
@@ -6,7 +6,10 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     path('',views.home,name='home'),
-    path('category_list',views.category_list,name='category_list')
+    path('category_list',views.category_list,name='category_list'),
+    path('brand_list',views.brand_list,name='brand_list'),
+    path('product-list', views.product_list, name='product-list')
+
 ]
 
 if settings.DEBUG:
