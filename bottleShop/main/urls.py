71fff526642891b9bc4d607e85e01df1f4b07028
merinjo90 <merinjo90@gmail.com>
2021-09-21 07:14:21 -0400

    1. Map the viwe into urls.py

--- bottleShop/main/urls.py
+++ bottleShop/main/urls.py
@@ -8,7 +8,9 @@ urlpatterns=[
     path('',views.home,name='home'),
     path('category_list',views.category_list,name='category_list'),
     path('brand_list',views.brand_list,name='brand_list'),
-    path('product-list', views.product_list, name='product-list')
+    path('product-list', views.product_list, name='product-list'),
+    path('category_product_list/<int:cat_id>',views.category_product_list,name='category_product_list'),
+    path('brand_product_list/<int:brand_id>', views.brand_product_list, name='brand_product_list')
 
 ]
 
