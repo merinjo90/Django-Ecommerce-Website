1d2d10b6b65e0aee058024b137207951d1cd2fb1
merinjo90 <merinjo90@gmail.com>
2021-09-21 01:57:03 -0400

    Fetch banner image in home page

--- bottleShop/main/views.py
+++ bottleShop/main/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
-from .models import Category, Brand, Product, ProductAttribute
+from .models import Banner, Category, Brand, Product, ProductAttribute
 
 
 # Create your views here.
 
 #Home Page
 def home(request):
+    banners=Banner.objects.all().order_by('-id')
     data=Product.objects.filter(is_featured=True).order_by('-id')
-    return render(request,'index.html',{'data':data})
+    return render(request,'index.html',{'data':data,'banners':banners})
 
 #Category
 def category_list(request):
