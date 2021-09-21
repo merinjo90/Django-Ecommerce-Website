e6e057341de48c7f103d9ecdb9d74ac78e432523
merinjo90 <merinjo90@gmail.com>
2021-09-21 14:08:54 -0400

    2. Define the URL path for the view

--- bottleShop/main/urls.py
+++ bottleShop/main/urls.py
@@ -10,7 +10,8 @@ urlpatterns=[
     path('brand_list',views.brand_list,name='brand_list'),
     path('product-list', views.product_list, name='product-list'),
     path('category_product_list/<int:cat_id>',views.category_product_list,name='category_product_list'),
-    path('brand_product_list/<int:brand_id>', views.brand_product_list, name='brand_product_list')
+    path('brand_product_list/<int:brand_id>', views.brand_product_list, name='brand_product_list'),
+    path('product/<str:slug>/<int:id>',views.product_detail,name='product_detail')
 
 ]
 
